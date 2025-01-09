1452350eeed35b8209ecf274320b65882789f94e
Tiago <tiagolch@hotmail.com>
2025-01-09 19:55:21 +0000

    whitenoise
    Signed-off-by: Tiago <tiagolch@hotmail.com>

--- main/settings.py
+++ main/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
 # Middleware
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -106,6 +107,7 @@ USE_TZ = True
 # Arquivos estáticos
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Arquivos de mídia (uploads)
 MEDIA_URL = '/media/'
