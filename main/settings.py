91f0e116d7c64a1fc329070cce87deb37254773f
Tiago <tiagolch@hotmail.com>
2025-01-09 19:01:13 +0000

    retirado prints
    Signed-off-by: Tiago <tiagolch@hotmail.com>

--- main/settings.py
+++ main/settings.py
@@ -79,11 +79,6 @@ DATABASES = {
     }
 }
 
-print(f"DB_NAME: {os.getenv('DB_NAME')}")
-print(f"DB_USER: {os.getenv('DB_USER')}")
-print(f"DB_PASSWORD: {os.getenv('DB_PASSWORD')}")
-print(f"DB_HOST: {os.getenv('DB_HOST')}")
-print(f"DB_PORT: {os.getenv('DB_PORT')}")
 
 # Validação de senha
 AUTH_PASSWORD_VALIDATORS = [
